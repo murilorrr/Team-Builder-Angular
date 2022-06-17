454d02fe83599384b4b83d95703229c5bd915c63
Murilo Ribeiro <murilorsv14@gmail.com>
2022-06-17 15:49:35 -0300

    feat: logic for generate teams and appers error messagem with inputs are incosistent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,21 @@ export class AppComponent {
   newMemberName = "";
   members: string[] = [];
   errorMessage = "";
+  numberOfTeams: number | "" = "";
+  teams: string[][] = [];
 
   addMember() {
     if (!this.newMemberName) {
-      this.triggerError();
+      this.triggerError("Name can't be empty");
     }
 
     this.members.push(this.newMemberName);
     this.changeInput('');
   }
 
-  private triggerError() {
-    const twoSeconds: number = 2000;
-    this.errorMessage = "Name can't be empty";
+  private triggerError(message: string) {
+    const twoSeconds = 2000;
+    this.errorMessage = message;
     setTimeout(() => {
       this.errorMessage = "";
     }, twoSeconds);
@@ -30,5 +32,34 @@ export class AppComponent {
 
   changeInput(member: string) {
     this.newMemberName = member;
+  }
+
+  setNumberOfTeams(numberOfTeams: string) {
+    this.numberOfTeams = parseInt(numberOfTeams, 10);;
+  }
+
+  generateTeams() {
+
+    if (this.numberOfTeams <= 0 || !this.numberOfTeams) {
+      this.triggerError("invalid number of teams");
+    }
+
+    const allMembers = [...this.members];
+    
+    while(allMembers.length) {
+      for (let index = 0; index < this.numberOfTeams; index++) {
+        const random = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(random, 1)[0];
+      
+        if (!member) break;
+        if (this.teams[index]) {
+          this.teams[index].push(member);
+        } else {
+          this.teams[index] = [member];
+        }
+      }
+    }
+    console.log(this.teams);
+    
   }
 }
