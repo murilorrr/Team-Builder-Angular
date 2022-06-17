bf791e1970c4ad911eb87bbf5115e03b47cd77c6
Murilo Ribeiro <murilorsv14@gmail.com>
2022-06-17 15:07:38 -0300

    :tada: initial model of team builder in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FAPP';
+  newMemberName = "";
+  members: string[] = [];
+  errorMessage = "";
+
+  addMember() {
+    if (!this.newMemberName) {
+      this.triggerError();
+    }
+
+    this.members.push(this.newMemberName);
+    this.changeInput('');
+  }
+
+  private triggerError() {
+    const twoSeconds: number = 2000;
+    this.errorMessage = "Name can't be empty";
+    setTimeout(() => {
+      this.errorMessage = "";
+    }, twoSeconds);
+  }
+
+  changeInput(member: string) {
+    this.newMemberName = member;
+  }
 }
